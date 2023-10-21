3566cf9856c57359261574ca0b7c9f5fda7ef4a3
olegsalabay <148639270+olegsalabay@users.noreply.github.com>
2023-10-21 18:13:16 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>LOGO</h1>
+      </header>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
